9a9718031a7f5c1af5af4920c60db9a7a88fe73c
wanglei <1130455004@qq.com>
2021-03-22 21:11:26 +0800

    登记式，静态内部类

--- app/src/main/java/com/wl/androidlearning/designpattern/singleton/Singleton.java
+++ app/src/main/java/com/wl/androidlearning/designpattern/singleton/Singleton.java
@@ -79,24 +79,59 @@ public class Singleton {
     //实现难度:较复杂
     //描述:这种方式采用双锁机制，安全且在多线程情况下保持高性能
 //    getInstance()的性能对应用程序很关键
-    private static volatile Singleton instance;
+//    private static volatile Singleton instance;
+//
+//    private Singleton(){
+//    }
+//
+//    public static Singleton getInstance(){
+//        if(instance==null){
+//            synchronized (Singleton.class){
+//                if(instance==null){
+//                    instance = new  Singleton();
+//                }
+//            }
+//        }
+//
+//        return instance;
+//    }
+//
+//        public void showMessage() {
+//        Log.d("singleton", "双检锁/双重校验锁");
+//    }
+
+
+    //5.登记式，静态内部类
+    //是否Lazy初始化:是
+    //是否多线程安全:是
+    //实现难度:一般
+    //描述:这种方式能达到双检锁方式一样的功效，但实现更简单。对静态域使用延迟初始化，
+//    应使用这种方式而不是双检锁方式。这种方式只适用于静态域的情况，双检锁
+//    方式可在实例域需要延迟初始化时使用。
+//    这种方式同样利用了classloader机制来保证初始化instance时只有一个线程，它跟第
+//    3种方式不同的是，第三种方式只要Singleton类被装载了，那么instance就会
+//    被实例化(没有达到lazyloading效果)，而这种方式是Singleton类被装载了，instance
+//    不一定被初始化。因为SingletonHolder类没有被主动使用，只有通过显式调用
+//    getInstance方法时，才会显示装载SingletonHolder类，从而实例化instance，想象一下，
+//    如果实例化instance很消耗资源，所以想让它延迟加载，另一方面，又不希望
+//    在Singleton类加载时就被实例化，因为不能确保Singleton类还可能在其他地方
+//    被主动使用从而被加载，那么这个时候实例化instance显然是不合适的。这个时候
+//    这种方式相比第三种方式就显得很合理
+//
+    private static class SingletonHolder{
+        private static Singleton INSTANCE = new Singleton();
+    }
 
     private Singleton(){
+
     }
 
     public static Singleton getInstance(){
-        if(instance==null){
-            synchronized (Singleton.class){
-                if(instance==null){
-                    instance = new  Singleton();
-                }
-            }
-        }
-
-        return instance;
+        return SingletonHolder.INSTANCE;
     }
 
-        public void showMessage() {
-        Log.d("singleton", "双检锁/双重校验锁");
+            public void showMessage() {
+        Log.d("singleton", "登记式，静态内部类");
     }
+
 }
